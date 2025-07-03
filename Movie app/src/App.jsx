ed71f0fc3db5ab754ea64906add9a16ca56feb17
Manu-jothish <manujothish722@gmail.com>
2025-07-03 13:04:13 +0530

    completed

--- Movie app/src/App.jsx
+++ Movie app/src/App.jsx
@@ -4,33 +4,83 @@ import "./App.css";
 import MovieList from "./components/movieList";
 import MovieListHandler from "./components/MovieListHandler";
 import SearchBox from "./components/SearchBox";
+import AddFavourite from "./components/AddFavourites";
+import RemoveFavourite from "./components/RemoveFavourite";
 
 function App() {
   const [movie, setMovie] = useState([]);
-  const [searchValue,setSearchValue]= useState("")
+  const [searchValue, setSearchValue] = useState("");
+  const [favourite, setFavourite] = useState([]);
 
   const getMovieRequest = async (searchValue) => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=99e376d`;
     const response = await fetch(url);
     const responsejson = await response.json();
-      if(responsejson.Search){
-     setMovie(responsejson.Search);
-      }
-    
+    if (responsejson.Search) {
+      setMovie(responsejson.Search);
+    }
   };
 
   useEffect(() => {
     getMovieRequest(searchValue);
-  }, [getMovieRequest]);
+  }, [searchValue]);
+
+  useEffect(() => {
+    const movieFavourites = JSON.parse(
+      localStorage.getItem("react-movie-app-favourite")
+    );
+    if (movieFavourites) {
+      setFavourite(movieFavourites);
+    }
+  }, []);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem("react-movie-app-favourite", JSON.stringify(items));
+  };
+
+  const addFavouriteMovie = (movie) => {
+    const newFavouriteList = [...favourite, movie];
+    setFavourite(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  };
+
+  const RemoveFromFavourite = (movie) => {
+    const newFavouriteList = favourite.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    );
+    setFavourite(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  };
 
   return (
-    <div className="movie-app container-fluid ">
-      <div className="row d-flex align-items-center mt-4 mb-4">
-        <MovieListHandler heading="Movies" />
-        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
-      </div>
-      <div className="row">
-        <MovieList movies={movie} />
+    <div className="movie-app container-fluid  ">
+      <div className="dh "style={{marginLeft: "90px"}}>
+        <div className="row  d-flex align-items-center mt-4 mb-4">
+          <MovieListHandler heading="Movies" />
+          <SearchBox
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+          />
+        </div>
+        <div className="row">
+          <MovieList
+            movies={movie}
+            handleFavouriteClick={addFavouriteMovie}
+            favouriteComponent={AddFavourite}
+          />
+        </div>
+
+        <div className="row d-flex align-items-center mt-4 mb-4">
+          <MovieListHandler heading="Favourites" />
+        </div>
+
+        <div className="row">
+          <MovieList
+            movies={favourite}
+            handleFavouriteClick={RemoveFromFavourite}
+            favouriteComponent={RemoveFavourite}
+          />
+        </div>
       </div>
     </div>
   );
--- Movie app/src/components/movieList.jsx
+++ Movie app/src/components/movieList.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 const MovieList = (props) => {
+
+  const FavouriteComponent = props.favouriteComponent;
+
   return (
     <>
       {props.movies.map((movie, index) => (
-        <div className="image-container col-12 col-md-4 mb-md-4 ">
-          <img src={movie.Poster} alt="movie-poster"></img>
-          <div className="overlay d-flex align-items-center justift-justify-content-center"></div>
+        <div key={index} className="image-container col-12 col-md-4 mb-md-4 ">
+          <img src={movie?.Poster} alt="movie-poster" />
+          <div onClick={() => props.handleFavouriteClick(movie)} className="overlay d-flex align-items-center justify-content-center">
+            <FavouriteComponent />
+          </div>
         </div>
       ))}
     </>
